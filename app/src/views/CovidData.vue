495fb6a6c43a325de585720686159626ed8385aa
JessL868 <144480854+JessL868@users.noreply.github.com>
2025-03-11 14:04:11 -0400

    YAYAYAYYA

--- app/src/views/CovidData.vue
+++ app/src/views/CovidData.vue
@@ -1,13 +1,13 @@
 import { onMounted } from 'vue';
 <template>
   <div>
-    <h2>{{ stat.date_of_interest }}</h2>
-      <h5>
-        Case Count: {{ stat.case_count }} <br>
-        Probable Case Count: {{ stat.probable_case_count }} <br>
-        Hospitalized Count: {{ stat.hospitalized_count }} <br>
-        Death Count: {{ stat.death_count }}
-      </h5>
+    <h2>{{ day.date_of_interest }}</h2>
+    <h5>
+      Case Count: {{ day.case_count }} <br>
+      Probable Case Count: {{ day.probable_case_count }} <br>
+      Hospitalized Count: {{ day.hospitalized_count }} <br>
+      Death Count: {{ day.death_count }}
+    </h5>
   </div>
 </template>
 
@@ -15,11 +15,11 @@ import { onMounted } from 'vue';
 import { ref, onMounted } from "vue";
 import { useRoute } from "vue-router";
 const route = useRoute();
-const days = ref("");
+const day = ref("");
 async function getData() {
   let res = await fetch(`https://data.cityofnewyork.us/resource/rc75-m7u3.json?date_of_interest=${route.params.date_of_interest}`);
   let data = await res.json();
-  days.value = data;
+  day.value = data;
 }
 onMounted(() => {
   getData();
