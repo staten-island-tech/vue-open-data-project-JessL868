05de7116299b8f1acfca152c189ae207eeb1848c
JessL868 <144480854+JessL868@users.noreply.github.com>
2025-03-18 14:01:50 -0400

    why

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -13,7 +13,7 @@ import { ref, onMounted } from "vue";
 import CovidDay from "../components/CovidDay.vue";
 const days = ref("");
 async function getData() {
-  let res = await fetch("https://data.cityofnewyork.us/resource/rc75-m7u3.json");
+  let res = await fetch("https://data.cityofnewyork.us/resource/rc75-m7u3.json?$limit=200");
   let data = await res.json();
   days.value = data;
 }
